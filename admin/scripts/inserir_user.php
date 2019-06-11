99a9bbb560ec3f501e31cbd579dd5d772dccb027
Alexandre Gamelas <you@example.com>
2019-06-11 22:07:22 +0100

    Insert Users Funcional

--- admin/scripts/inserir_user.php
+++ admin/scripts/inserir_user.php
@@ -2,6 +2,7 @@
 session_start();
 include_once "../connections/connection.php";
 var_dump($_POST);
+echo "<br>";
 $nome = $_POST['nome'];
 $mail = $_POST['mail'];
 $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
@@ -12,7 +13,7 @@ $stmt = mysqli_stmt_init($link);
 $query = "INSERT INTO utilizadores (nome, email, password, data_nascimento) VALUES (?, ?, ?, ?) ";
 
 if (mysqli_stmt_prepare($stmt, $query)) { // Prepare the statement
-    mysqli_stmt_bind_param($stmt, 'ssss', $nomeP,$mailP, $passwordP, $data_nascimentoP);
+    mysqli_stmt_bind_param($stmt, 'ssss', $nomeP,$emailP, $passwordP, $data_nascimentoP);
     $nomeP = $nome;
     $emailP = $mail;
     $passwordP = $password;
@@ -21,11 +22,13 @@ if (mysqli_stmt_prepare($stmt, $query)) { // Prepare the statement
    if (mysqli_stmt_execute($stmt)){
         header("Location: ../tabela_utilizadores.php");
    } else {
+       echo mysqli_stmt_error($stmt);
        echo "<p>Erro no execute</p>";
    }
 
     mysqli_stmt_close($stmt); // Close statement
 } else {
+    echo mysqli_stmt_error($stmt);
      mysqli_error($link);
 }
 
