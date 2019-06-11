00e6ecd322a3cc0743f1f51b6bc6e77f6404ec1e
Alexandre Gamelas <you@example.com>
2019-06-11 22:01:54 +0100

    Inseet users (com erro)

---
+++ admin/scripts/inserir_user.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+include_once "../connections/connection.php";
+var_dump($_POST);
+$nome = $_POST['nome'];
+$mail = $_POST['mail'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+$nascimento = $_POST['nascimento'];
+
+$link = new_db_connection();
+$stmt = mysqli_stmt_init($link);
+$query = "INSERT INTO utilizadores (nome, email, password, data_nascimento) VALUES (?, ?, ?, ?) ";
+
+if (mysqli_stmt_prepare($stmt, $query)) { // Prepare the statement
+    mysqli_stmt_bind_param($stmt, 'ssss', $nomeP,$mailP, $passwordP, $data_nascimentoP);
+    $nomeP = $nome;
+    $emailP = $mail;
+    $passwordP = $password;
+    $data_nascimentoP = $nascimento;
+
+   if (mysqli_stmt_execute($stmt)){
+        header("Location: ../tabela_utilizadores.php");
+   } else {
+       echo "<p>Erro no execute</p>";
+   }
+
+    mysqli_stmt_close($stmt); // Close statement
+} else {
+     mysqli_error($link);
+}
+
+mysqli_error($link);
+mysqli_close($link);
