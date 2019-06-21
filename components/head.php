715d1431dd16f2c7802c38e4cf9840d610148dea
J-Brandao <jp.brandao@ua.pt>
2019-06-21 17:01:42 +0100

    FontAwesome FIX

--- components/head.php
+++ components/head.php
@@ -13,7 +13,8 @@
 
     <!-- FontAwesome + Animações + Fonts-->
     <link rel="stylesheet" href="css/animate.css">
-    <link rel="stylesheet" href="css/fontawesome-all.css">
+    <!--<link rel="stylesheet" href="css/fontawesome-all.css">-->
+    <link href="css/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link href=" https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
 
